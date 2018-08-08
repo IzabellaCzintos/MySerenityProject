7cbd254d9e1c53e764d7362647a75253896a8f52
c.izabella <c.izabellaaa@gmail.com>
2018-08-08 14:51:03 +0300

    added new constant

--- src/test/java/com/evozon/features/search/Constants.java
+++ src/test/java/com/evozon/features/search/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
 	public String BASE_URL="http://qa3.dev.evozon.com/";
 	public static final String CSV_FILES_PATH="src/test/resources/resources/";
+	public static final String CSV_FILES_PATH2="src/test/resources/resources/2";
 
 	
 }
